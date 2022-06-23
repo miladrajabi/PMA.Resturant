1beff90e941aa29e58545013ba1fa617d8e98277
Milad <rajabi.milad@gmail.com>
2022-06-23 17:18:30 +0430

    Develop

---
+++ Resturants.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace Resturants.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task<T> PostAsync<T>();
+        Task<T> PutAsync<T>();
+        Task<T> DeleteAsync<T>(int id);
+    }
+}
